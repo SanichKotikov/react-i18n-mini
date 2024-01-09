b396080d05ea00a64336925c5cb9b1e982f93f91
Sanich <sancho@cdrpro.ru>
2024-01-09 13:38:25 +0300

    Fixed types

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { createElement, Children, Fragment } from 'react';
 import type { I18nOptions } from 'i18n-mini';
 import type { I18nMessage, I18nValues } from 'i18n-mini/lib/types';
 import { createI18n as create } from 'i18n-mini';
+import type { I18nReturn } from 'i18n-mini/lib/core';
 
 export { useI18n } from './context';
 export { I18nProvider } from './components/I18nProvider';
@@ -22,13 +23,15 @@ function formatTag(tag: string, child: string | string[] | undefined) {
   return createElement(tag, null, child);
 }
 
-export function createI18n(options: Omit<I18nOptions, 'formatTag'>) {
-  const { i18n, subscribe } = create<string | ReactFragment>({ ...options, formatTag });
+type TFormatType = string | ReactFragment;
+
+export function createI18n(options: Omit<I18nOptions, 'formatTag'>): Readonly<I18nReturn<TFormatType>> {
+  const { i18n, subscribe } = create<TFormatType>({ ...options, formatTag });
 
   return {
     i18n: {
       ...i18n,
-      t: (message: string | I18nMessage, values?: Readonly<I18nValues>) => {
+      t: (message: string | I18nMessage, values?: Readonly<I18nValues>): TFormatType | TFormatType[] => {
         const result = i18n.t(message, values);
 
         return Array.isArray(result)
